d8fc90ee7127eaba0bdb6a438a2e06a1613cdd3c
Simon Duquette <simon.duquette@hotmail.com>
2023-05-06 11:08:01 -0400

    commit test within android studio

--- app/src/main/java/workout/builder/MainActivity.kt
+++ app/src/main/java/workout/builder/MainActivity.kt
@@ -3,6 +3,7 @@ package workout.builder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//Committing a comment to test git
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
