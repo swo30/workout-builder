0ab8e3f3db17a6c484fc150e72b4421c4eb4ba09
Kevin Duguay <kev.duguay96@gmail.com>
2023-05-06 11:41:39 -0400

    Ass penis

--- app/src/main/java/workout/builder/MainActivity.kt
+++ app/src/main/java/workout/builder/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 //Committing a comment to test git
+//Ass penis
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
